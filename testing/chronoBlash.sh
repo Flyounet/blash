4da1ca40a1e9e3dda7924206caebd22e2732b071
Flyounet <dev+github@flyou.net>
2016-11-02 21:27:11 +0100

    Cosmetic changes. The chronoBlash script has been updated to take into account cosmetic changes.

--- testing/chronoBlash.sh
+++ testing/chronoBlash.sh
@@ -16,38 +16,56 @@ POUET
 
 uts_deb="$(date '+%s')"
 ../blash -b ${@} 2>&1 | while read line; do
-	if [[ "${line,,}" =~ ^rendering[[:space:]]posts.*$ ]]; then
-		uts_render="$(date '+%s')"
+	# The line starts with a number so it's a post
+	if [[ "${line,,}" =~ ^[[:digit:]][[:digit:]]*.*$ ]]; then
+		((posts++))
+	fi
+	if [[ "${line,,}" =~ ^.*loading[[:space:]]posts.*$ ]]; then
+		uts_0="$(date '+%s')"
 		nb=0
 		echo "Starting..."
 	fi
-	if [[ "${line,,}" =~ ^rendering[[:space:]]rss$ ]]; then
-		uts_rss="$(date '+%s')"
-		((posts=nb-2))
-		echo "Rendering time : $((uts_rss-uts_render))s for ${posts} posts (overall: $(ls -1 posts |wc -l))"
-		uts_rss="$(date '+%s')"
+	if [[ "${line,,}" =~ ^.*draft[[:space:]]posts.*$ ]]; then
+		uts_1="$(date '+%s')"
+		posts=0
+		echo "Loading time : $((uts_1 - uts_0))s"
 	fi
-	if [[ "${line,,}" =~ ^rendering[[:space:]]atom$ ]]; then
-		uts_atom="$(date '+%s')"
-		echo "Rendering time : $((uts_atom-uts_rss))s for RSS feed"
-		uts_atom="$(date '+%s')"
+	if [[ "${line,,}" =~ ^.*hidden[[:space:]]posts.*$ ]]; then
+		uts_2="$(date '+%s')"
+		echo "Draft posts to render ${posts} in $((uts_2 - uts_1))s"
+		posts=0
 	fi
-	if [[ "${line,,}" =~ ^rendering[[:space:]]index\.*$ ]]; then
-		uts_idx="$(date '+%s')"
-		echo "Rendering time : $((uts_idx-uts_atom))s for Atom feed"
-		uts_idx="$(date '+%s')"
+	if [[ "${line,,}" =~ ^.*regular[[:space:]]posts.*$ ]]; then
+		uts_3="$(date '+%s')"
+		echo "Hidden posts to render ${posts} in $((uts_3 - uts_2))s"
+		posts=0
 	fi
-	if [[ "${line,,}" =~ ^rendering[[:space:]]index[[:space:]]for[[:space:]]categories.*$ ]]; then
-		uts_tag="$(date '+%s')"
-		echo "Rendering time : $((uts_tag-uts_idx))s for index.html file"
-		uts_tag="$(date '+%s')"
+	if [[ "${line,,}" =~ ^.*rss[[:space:]]feed.*$ ]]; then
+		uts_4="$(date '+%s')"
+		echo "Regular posts to render ${posts} in $((uts_4 - uts_3))s"
+		posts=0
+	fi
+	if [[ "${line,,}" =~ ^.*atom[[:space:]]feed.*$ ]]; then
+		uts_5="$(date '+%s')"
+		echo "RSS feed rendered in $((uts_5 - uts_4))s"
+		posts=0
+	fi
+	if [[ "${line,,}" =~ ^.*index[[:space:]]:.*$ ]]; then
+		uts_6="$(date '+%s')"
+		echo "Atom feed rendered in $((uts_6 - uts_5))s"
+		posts=0
+	fi
+	if [[ "${line,,}" =~ ^.*categories[[:space:]]:.*$ ]]; then
+		uts_7="$(date '+%s')"
+		echo "Index rendered in $((uts_7 - uts_6))s"
+		posts=0
 	fi
 	if [[ "${line,,}" =~ ^blashed.*$ ]]; then
-		uts_end="$(date '+%s')"
-		echo "Rendering time : $((uts_end-uts_tag))s for $(ls -1 out/index_*.html|wc -l) index_tags.html files"
+		uts_8="$(date '+%s')"
+		echo "Categories rendered in $((uts_8 - uts_7))s"
+		posts=0
 	fi
 	((nb++))
-#done <<< "$(../blash -f ./blash.conf -b 2>&1)"
 done
 uts_fin="$(date '+%s')"
 
